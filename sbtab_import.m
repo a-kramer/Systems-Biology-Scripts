c2856ec59c9a3138723f21302da3666f9b5442b5
Andrei Kramer <andreikr@kth.se>
2020-02-26 23:52:38 +0100

    tested in matlab; seems to work OK for simple examples (tsv files I wrote for the CaMKII model)

--- sbtab_import.m
+++ sbtab_import.m
@@ -4,7 +4,7 @@ function [sbtab] = sbtab_import(tsv_file)
   %%
   %%   input: tsv_file is a character array with the file name to import (a tab separated values [text file])
   %%  output: a structure array with fields corresponding to column headers
-  assert(exist(tsv_file,'file'));
+  assert(logical(exist(tsv_file,'file')));
   tsv=fopen(tsv_file);
   %% Document Name
   str=fgetl(tsv);
@@ -13,12 +13,13 @@ function [sbtab] = sbtab_import(tsv_file)
   fprintf('Importing from file «%s» part of Document «%s»\n',tsv_file,DocumentName);
   %% Header
   str=fgetl(tsv);
-  fprintf('processing column header: «%s»\n',str);
+  fprintf('processing column header: «%s»\n',regexprep(str,'\t',' '));
   header=linetocstr(str);
   assert(~isempty(header));
+  header=regexprep(header,'[^A-Za-z0-9]','');
   N=length(header);
   sbtab=struct();
-  fprintf('The header contains the fields: ');
+  fprintf('The header contains the cleaned up fields: ');
   for i=1:N
     fprintf('«%s»',header{i});
   end%for
@@ -31,9 +32,16 @@ function [sbtab] = sbtab_import(tsv_file)
     for j=1:length(cstr)
       d=str2double(cstr{j});
       if isnan(d)
-	sbtab(i).(header{j})=cstr{j};	
+          switch(cstr{j})
+              case {'FALSE','false'}
+                  sbtab(i).(header{j})=false;
+              case {'TRUE','true'}
+                  sbtab(i).(header{j})=true;
+              otherwise
+                  sbtab(i).(header{j})=cstr{j};
+          end
       else
-	sbtab(i).(header{j})=d;
+	    sbtab(i).(header{j})=d;
       end%if
     end%for
     i=i+1;
@@ -42,11 +50,13 @@ function [sbtab] = sbtab_import(tsv_file)
 end%function
 
 function [cstr]=linetocstr(str)
-  cstr=[];
+  cstr=strsplit(str,'%');
+  str=cstr{1};
   not_a_comment=isempty(regexp(str,'^\\s*%','start','once'));
   if not_a_comment
-    cstr=ostrsplit(str,'\t');
+    cstr=strsplit(str,'\t','CollapseDelimiters',false);
   else
     fprintf('skipping comment line: «%s»\n',str);
+    cstr=[];
   end%if    
 end%function

282e187157ff250d2b7243ca88c559f81857ab69
Andrei Kramer <kramer.ndr@gmail.com>
2020-06-04 20:14:54 +0200

    reactions are now numbered if they don't have names, the time variable is fixed as the sbml standard wants it: 'csymbol' etc.

--- sbml_fix.sh
+++ sbml_fix.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# (C) Andrei Kramer, andreikr@kth.se
+
+# DISCLAIMER: 
+# the author does not like or understand sbml
+
 
 FILE=${1}
 
@@ -10,6 +15,10 @@ IFS=$'\n' lines=( `egrep "<$tag[ ]" ${FILE}` )
 n=${#lines[@]}
 for ((i=0;i<n;i++)); do
     svstr="${lines[i]}"
+    # number of digits
+    d=${#n}
+    # write index i as 0001 etc. if necessary
+    I=`printf "%0${d}i" $i`
     if [[ $svstr =~ id=\"([^\"]+)\" ]]; then
 	id[i]="${BASH_REMATCH[1]}"
     else
@@ -19,12 +28,22 @@ for ((i=0;i<n;i++)); do
 	name[i]="${BASH_REMATCH[1]}"
     else
 	echo "warning: name not found in «${svstr}»"
-	name[i]="${tag}_${i}"
+	name[i]="${tag}_${I}"
     fi
     if [[ "${id[i]}" ]]; then
 	sed -i -e "s/${id[i]}/${name[i]}/g" ${FILE}
     fi
 done
+
+# part of the sbml insanity is this:
+# <math xmlns="http://www.w3.org/1998/Math/MathML"> 
+# according to the FAQ (http://sbml.org/Documents/FAQ#What_is_the_symbol_for_time_in_SBML.3F) 
+# time is this in sbml:      
+#      <csymbol encoding="text" definitionURL="http://www.sbml.org/sbml/symbols/time"> 
+#           t 
+#      </csymbol> 
+sed -E -i -e 's/<ci>\s*time\s*<\/ci>/<csymbol encoding="text" definitionURL="http:\/\/www.sbml.org\/sbml\/symbols\/time">time<\/csymbol>/g' ${FILE}
+
 
 echo "id: ${id[*]}"
 echo "name: ${name[*]}"

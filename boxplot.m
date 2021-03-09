5c9f8b965f460a3878ed92732ba78ccc21b71325
Andrei Kramer <kramer.ndr@gmail.com>
2021-03-10 00:02:35 +0100

    made the boxplot function more flexible and the octavetomatlab script not replace «end» if it is used as a vector index

--- boxplot.m
+++ boxplot.m
@@ -1,36 +1,84 @@
 function [h]=boxplot(x,y,varargin)
   ## Usage: boxplot(x,y,KEY,VALUE)
   ##
+  ## KEYS (long and short)
+  ## ----
+  ## probability
+  ##          p: a vector of cumulative
+  ##             probabiity values for
+  ##             quantile calculation
+  ##             default is
+  ##             p = [0,0.25,0.5,0.75,1]
+  ## quantile
+  ##          q: pre-calculated
+  ##             quantiles (p not needed)
+  ##
+  ## box width, width
+  ##          w: a factor for the width
+  ##             of the boxes (relative)
+  ##             defaults to 1.0
+  ##
+  ##
+  ## face color,fill,patch
+  ##         fc: box fill color
+  ##             defaults to white
+  ##
+  ## edge color, box color
+  ##         bc: box edge color
+  ##             defaults to blue
+  ##
+  ## median color, median
+  ##         mc: color of the median line
+  ##             defaults to red
+  ##
+  ## KEYS can have spaces and capital letters in them
   n=nargin-2; # (key,value) pairs
   p=[0,0.25,0.5,0.75,1]; #default
   bc="blue";
   mc="red";
-  for i=1:2:n
-    switch(varargin{i})
-      case {"quantile","Quantile","p","P"}
+  fc="white";
+  BoxWidth=1;
+  assert(mod(n,2)==0);
+  Q=[];
+   for i=1:2:n
+    switch(strrep(lower(varargin{i})," ",""));
+      case {"p","probability","cumulativeprobability"}
 	p=varargin{i+1};
 	assert(length(p)==5);
-      case {"median color"}
-	mc=varargin{i+1};
-	assert(isvector(mc) && length(mc)==3);
-      case {"box color"}
+      case {"q","quantile"}
+	Q=varargin{i+1}';
+      case {"w","width","boxwidth"}
+	BoxWidth = varargin{i+1};
+	assert(isscalar(BoxWidth));
+      case {"facecolor","fill","patch","fc"}
+	fc=varargin{i+1};
+	assert(isvector(fc) && length(fc)>=3);
+      case {"edgecolor","boxcolor","bc","ec"}
 	bc=varargin{i+1};
-	assert(isvector(bc) && length(bc)==3);
+	assert(isvector(bc) && length(bc)>=3);
+      case {"median","mediancolor","mc"}
+	mc=varargin{i+1};
+	assert(isvector(mc) && length(mc)>=3);
       otherwise
-	error("unknown option: %s",varargin{1});
+	error("unknown option: %s",varargin{i});
     endswitch
   endfor
-  assert(isvector(x) && ismatrix(y) &&length(x)==size(y,2));
-  Q=quantile(y,p)';
+
+  if isempty(Q)
+    assert(isvector(x) && ismatrix(y) && length(x)==size(y,2));
+    Q=quantile(y,p)';
+  else
+    assert(size(Q,2)==5 && size(Q,1)==length(x));
+  endif
   h=struct();
-  D=diff(x);
+  D=diff(x)*BoxWidth;
   D=cat(2,D,D(end));
-  clf;
+  #clf;
   hold on;
-  for j=1:size(y,2)
+  for j=1:size(Q,1)
     d=D(j)*0.4;
     h(j).whisker=plot(x(j)*ones(1,2),Q(j,[1,5]),"color",bc);
-    h(j).box=patch(x(j)+[-d,+d,+d,-d],[Q(j,[2,2]),Q(j,[4,4])],"edgecolor",bc,"facecolor","white");
+    h(j).box=patch(x(j)+[-d,+d,+d,-d],[Q(j,[2,2]),Q(j,[4,4])],"edgecolor",bc,"facecolor",fc);
     h(j).median=plot(x(j)+[-d,+d],Q(j,[3,3]),"color",mc);
   endfor
   hold off;
--- octavetomatlab.sh
+++ octavetomatlab.sh
@@ -3,16 +3,18 @@
 INFILE=${1:-main.m}
 OUTFILE=${2:-matlab_${INFILE}}
 
-if [[ -z ${2} ]]; then
+if [[ -z ${OUTFILE} ]]; then
    OPT="-i"
    echo "performing the substitutions in place"
 fi
    
 if [[ -f "$INFILE" ]]; then
     sed -E ${OPT} \
-    --expression='s/\<end/end%/' \
+    --expression='s/\<end\B/end%/' \
     --expression='s/#/%/g' \
+    --expression='s/%%/%/g' \
     --expression="s/\"/'/g" \
+    --expression='s/,$/,.../' \
     --expression='s/\<printf\(/fprintf(/g' \
     --expression='s/\<columns\((\w+)\)/size(\1,2)/g' \
     --expression='s/\<rows\((\w+)\)/size(\1,1)/g' \
